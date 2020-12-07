771d3cf6df621bb5d65eed636d75236e74939771
davidjguru <davidjguru@gmail.com>
2020-12-07 20:14:03 +0100

    (PHP) - Adds more info about realistic values.

--- php_performance/show_memory_usage_using_variables.php
+++ php_performance/show_memory_usage_using_variables.php
@@ -9,7 +9,13 @@
 
 
 // Unique variable.
+   $memory_first = memory_get_usage();
    $var = 'Lorem fistrum por la gloria de mi madre por la gloria de mi madre apetecan jarl.';
+   echo 'Length of the string contained in the initial variable: ' . strlen($var), "\n";
+   echo 'Initial Memory consumption, non-real in bytes:  ' . (memory_get_usage() - $memory_first) , "\n";
+   echo 'Initial Memory consumption, non-real in bits: ' . ((memory_get_usage() - $memory_first) * 8), "\n"; 
+   echo 'Initial variable size, STRLEN in bytes: ' . strlen($var) , "\n";
+   echo 'Initial variable size, using bits: ' . (strlen($var) * 8) , "\n";
 
 // Gets the initial memory consumption.
   $baseMemory = memory_get_usage();
@@ -17,7 +23,7 @@
 // Inits the loop.
   for ( $i = 0; $i <= 100000; $i++ )
   {
-      $variable_iteration_ . $i = $var;
+      ${'variable_iteration_' . $i} = $var;
       
       // Gets current memory usage and loads it in csv file.
       $current_memory_usage = memory_get_usage() - $baseMemory;
